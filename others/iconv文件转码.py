5174966de0a1fb0eba435ba0558409f569029ef6
hsingpu <1927833039@qq.com>
2021-09-10 15:04:14 +0800

    目录梳理

---
+++ others/iconv文件转码.py
@@ -0,0 +1,10 @@
+with open(r"C:\Users\hsingpu\Documents\WeChat Files\windfishing\FileStorage\File\2021-08\new9.csv", mode='r',
+          encoding='gbk') as oldfile:
+    nfile = open(r"C:\Users\hsingpu\Documents\WeChat Files\windfishing\FileStorage\File\2021-08\new92.csv", mode='a',
+                 encoding='utf-8')
+    for line in oldfile.readlines():
+        print(line)
+        nfile.write(line)
+    # for l in oldfile.readline():
+    nfile.close()
+    #         nfile.writelines(line)
